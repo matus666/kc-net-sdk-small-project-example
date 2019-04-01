fb420263700704642ce6e89a3d54cc770074d85d
Simply007 <chrastina.ondra@gmail.com>
2019-04-01 15:46:17 -0400

    Add project files.

---
+++ kc-net-sdk-small-project-example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kc_net_sdk_small_project_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
